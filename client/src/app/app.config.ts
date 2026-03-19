a26f07de0e2ce2e7a07d9b47574287fd3b594a30
canopyfox <228879377+canopyfox@users.noreply.github.com>
2026-03-18 21:07:16 -0400

    Add Sell link to navbar

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -6,7 +6,7 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { provideIcons, provideNgIconsConfig } from '@ng-icons/core';
-import { lucideArrowLeft, lucideBell, lucideBookmark, lucideCamera, lucideCircleUserRound, lucideGraduationCap, lucideHome, lucideLeaf, lucideLogOut, lucideMenu, lucideMessageCircle, lucidePocket, lucideSearch, lucideSend, lucideX } from '@ng-icons/lucide';
+import { lucideArrowLeft, lucideBell, lucideBookmark, lucideCamera, lucideCircleUserRound, lucideGraduationCap, lucideHome, lucideLeaf, lucideLogOut, lucideMenu, lucideMessageCircle, lucidePlusCircle, lucidePocket, lucideSearch, lucideSend, lucideX } from '@ng-icons/lucide';
 
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
@@ -16,6 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     provideNgIconsConfig({}),
-    provideIcons({ lucideArrowLeft, lucideBell, lucideBookmark, lucideCamera, lucideCircleUserRound, lucideGraduationCap, lucideHome, lucideLeaf, lucideLogOut, lucideMenu, lucideMessageCircle, lucidePocket, lucideSearch, lucideSend, lucideX })
+    provideIcons({ lucideArrowLeft, lucideBell, lucideBookmark, lucideCamera, lucideCircleUserRound, lucideGraduationCap, lucideHome, lucideLeaf, lucideLogOut, lucideMenu, lucideMessageCircle, lucidePlusCircle, lucidePocket, lucideSearch, lucideSend, lucideX })
   ]
 };
